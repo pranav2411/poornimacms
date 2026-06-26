670f217630cc2d019e8cf5b07d508e2310e8a318
Moxit Rewar <mate022814@gmail.com>
2026-06-26 20:50:19 +0530

    Env Var Fix

--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -1,13 +1,16 @@
 import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
-const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
-const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || (appId ? appId.split(":")[1] : undefined);
+const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID || process.env.NEXT_PUBLIC_FIREBASE_APPID;
+const messagingSenderId =
+  process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||
+  process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID ||
+  (appId ? appId.split(":")[1] : undefined);
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
   appId: appId,
   messagingSenderId: messagingSenderId,
 };
